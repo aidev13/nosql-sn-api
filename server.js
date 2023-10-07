b52afbc7ba2aad204b3734e9c09f1479fa6b9ab3
David Liebherr <ambition.aidev@gmail.com>
2023-10-06 20:43:08 -0500

    WIP, create and confirmed mongodata

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const { app } = require('./config/connection')
-const { MongoClient } = require('mongodb')
 
+const { MongoClient } = require('mongodb')
 const url = 'mongodb://127.0.0.1:27017'
 const client = new MongoClient(url)
-
 const dbName = 'social_db'
 
 const init = async () => {
-    await client.connect()
-    console.log("MongoDB Connected!")
+    await client.connect();
+    console.log("MongoDB Connected!");
+    const db = client.db(dbName);
+    const socialCollection = db.collection('social');
+    const socialData = await socialCollection.find().toArray();
 
-    const db = client.db(dbName)
+    console.log(socialData)
 }
 
 init()
